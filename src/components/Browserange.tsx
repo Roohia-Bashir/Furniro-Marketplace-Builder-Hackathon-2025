2c29e87bd488d20d8693aa92abcaa03ae8b8c839
Roohia-Bashir <roohiabashir1994@gmail.com>
2025-02-07 02:59:20 +0500

    Updated Browserange.tsx file

--- src/components/Browserange.tsx
+++ src/components/Browserange.tsx
@@ -2,43 +2,53 @@ import React from "react";
 import Image from "next/image";
 import { browseRange } from "@/constant/browseRange";
 
-function BrowseRange() {
+function Browserange() {
   return (
-    <section className="mt-14 px-4 sm:px-8 md:px-16 lg:px-20 xl:px-28 max-w-7xl mx-auto mb-40 font-poppins">
-      {/* Top Heading */}
-      <div className="flex flex-col items-center text-center">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-black">
-          Browse The Range
-        </h1>
-        <p className="text-lg sm:text-xl text-black mt-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </p>
-      </div>
+    <>
+      <section className="mt-[56.47px] w-[1183px] pl-[131px] pr-[126px] m-auto mb-40 font-poppins">
+        {/* top heading */}
+        <div className="flex justify-center flex-col items-center">
+          <h1 className="text-[32px] leading-[48px] font-bold text-black text-center">
+            Browse The Range
+          </h1>
+          <p className="text-[20px] leading-[30px] text-black text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
 
-      {/* Card Body */}
-      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 place-items-center">
-        {browseRange.map((item, index) => (
-          <div className="flex flex-col items-center" key={index}>
-            {/* Image */}
-            <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg h-auto relative">
-              <Image
-                src={item.src}
-                alt={item.name}
-                width={400} // Default size
-                height={500} // Default size
-                className="w-full h-auto rounded-lg object-cover"
-              />
-            </div>
+        {/* card body */}
 
-            {/* Card Text */}
-            <h2 className="mt-4 text-lg sm:text-xl font-semibold text-black">
-              {item.name}
-            </h2>
-          </div>
-        ))}
-      </div>
-    </section>
+        <div className="mt-[62.29px] flex  items-center  justify-center  gap-[20px] w-full ">
+          {browseRange.map((item, index) => {
+            return (
+              <div
+                className="flex items-center justify-center flex-col"
+                key={index}
+              >
+                {/* top image */}
+                <div className="w-[381px] h-[480px] ">
+                  <Image
+                    src={item.src}
+                    alt="dining"
+                    width={"381"}
+                    height={"480"}
+                  ></Image>
+                </div>
+
+                {/* card text */}
+
+                <div>
+                  <h1 className="text-[24px] leading-[36px] font-semibold text-black text-center">
+                    {item.name}
+                  </h1>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
   );
 }
 
-export default BrowseRange;
+export default Browserange;
